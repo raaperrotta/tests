6f708b3b6256d03eb7e1dd62d1d080ec13466b56
Robert Perrotta <raaperrotta@gmail.com>
2016-08-26 12:43:40 -0400

    Save WORKSPACE as wrk
    Instead of getting the environment variable each time, tapTests now
    saves it as wrk at top of file.

--- tapTests.m
+++ tapTests.m
@@ -3,7 +3,9 @@ import matlab.unittest.TestRunner;
 import matlab.unittest.plugins.TAPPlugin;
 import matlab.unittest.plugins.ToFile;
 
-cd(getenv('WORKSPACE'))
+wrk = getenv('WORKSPACE'); % Jenkins environment variable
+
+cd(wrk)
 
 thisversion = ['R',version('-release')];
 if strcmp(thisversion,'R2015a') && ~verLessThan('matlab','8.5.1')
@@ -11,11 +13,11 @@ if strcmp(thisversion,'R2015a') && ~verLessThan('matlab','8.5.1')
 end
 
 try
-    suite = TestSuite.fromFolder(fullfile(getenv('WORKSPACE')));
+    suite = TestSuite.fromFolder(fullfile(wrk));
     % Create a typical runner with text output
     runner = TestRunner.withTextOutput();
     % Add the TAP plugin and direct its output to a file
-    tapFile = fullfile(getenv('WORKSPACE'),[thisversion,'TestResults.tap']);
+    tapFile = fullfile(wrk,[thisversion,'TestResults.tap']);
     runner.addPlugin(TAPPlugin.producingOriginalFormat(ToFile(tapFile)));
     % Run the tests
     results = runner.run(suite);

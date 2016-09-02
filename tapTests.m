78e0dee384b011e0a2e4509bc90bcf72972d5efd
Robert Perrotta <raaperrotta@gmail.com>
2016-09-02 06:27:56 -0400

    made tapTests a function

--- tapTests.m
+++ tapTests.m
@@ -1,11 +1,17 @@
+function tapTests(name)
+
 import matlab.unittest.TestSuite;
 import matlab.unittest.TestRunner;
 import matlab.unittest.plugins.TAPPlugin;
 import matlab.unittest.plugins.ToFile;
 
+if nargin == 0
+    name = ''; % run all tests
+end
+
 wrk = getenv('WORKSPACE'); % Jenkins environment variable
 
-cd(wrk) 
+cd(wrk)
 
 thisversion = ['R',version('-release')];
 if strcmp(thisversion,'R2015a') && ~verLessThan('matlab','8.5.1')
@@ -13,11 +19,11 @@ if strcmp(thisversion,'R2015a') && ~verLessThan('matlab','8.5.1')
 end
 
 try
-    suite = TestSuite.fromFolder(fullfile(wrk));
+    suite = TestSuite.fromFolder(fullfile(wrk,name));
     % Create a typical runner with text output
     runner = TestRunner.withTextOutput();
     % Add the TAP plugin and direct its output to a file
-    tapFile = fullfile(wrk,[thisversion,'TestResults.tap']);
+    tapFile = fullfile(wrk,[name,'_',thisversion,'_TestResults.tap']);
     runner.addPlugin(TAPPlugin.producingOriginalFormat(ToFile(tapFile)));
     % Run the tests
     results = runner.run(suite);
